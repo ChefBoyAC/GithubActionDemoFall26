b5f8a2ce71f3d77d9528e64c4e9fea7e2b60e94d
Aaron <79734592+ChefBoyAC@users.noreply.github.com>
2026-09-04 13:00:39 -0400

    added 2 calculator files

---
+++ CalculatorTests.java
@@ -0,0 +1,12 @@
+
+public class CalculatorTests {
+   public static void main(String[] args)
+   {
+        if(Calculator.add(2,3) != 5)
+        {
+            throw new AssertionError("2 + 3 test case failed.");
+        }
+
+        System.out.println("All test cases passed");
+   }
+}
